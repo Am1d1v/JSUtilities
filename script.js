7f9df11c7c8fed8a761eff22266e6c605ff52ebc
Am1d1v <124504237+Am1d1v@users.noreply.github.com>
2023-09-28 20:46:44 +0300

    Add files via upload

--- script.js
+++ script.js
@@ -1,34 +1,31 @@
 
 
-let container = document.getElementById("container");
-let colors = ["#341ba3", "#251e44", "#765bee", "#ae00ff", "#541c6e", "#2b043d"];
-let SQUARES = 600;
 
-for(let i = 0; i < SQUARES; i++){
-    let square = document.createElement("div");
-    square.classList.add("square");
+let insert = document.getElementById("insert");
 
 
-    square.addEventListener("mouseover", ()=> setColor(square));
+window.addEventListener("keydown",(e)=>{
+   // console.log(event);
+   // console.log(event.key);
+   // console.log(event.keyCode);
+   //  console.log(event.code);
 
-    square.addEventListener("mouseout", ()=> removeColor(square));
+    insert.innerHTML = `
+    <div class="key">
+        ${e.key === " " ? "Space" : e.key}
+         <small>event.key</small>
+    </div>
+    
 
-    container.append(square);
-}
+    <div class="key">
+        ${e.keyCode}
+        <small>event.keyCode</small>
+    </div>
 
-function setColor(element){
-    let color = getRandomColor()
-    console.log(color);
-    element.style.background = color;
-    element.style.boxShadow = `0 0 200px ${color}, 0 0 100px ${color}`
-}
+    <div class="key">
+    ${e.code}
+        <small>event.code</small>
+    </div>
+    `
+});
 
-function removeColor(element){
-    element.style.background = "#313131";
-    element.style.boxShadow = "0 0 2px black";
-
-}
-
-function getRandomColor(){
-    return colors[Math.floor(Math.random() * colors.length)];
-}
